e8ac98f154a4a164679d88fd0a5676601a9dcceb
Tymothy Sellers <tsellers1990@gmail.com>
2020-08-08 17:06:39 -0600

    used react.memo for the cell export to make it load faster

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,45 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { createStage, checkCollision } from '../gameHelpers';
+import { createStage, checkCollision } from "../gameHelpers";
 
 // Styled Components
-import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 // Custom Hooks
-import { usePlayer } from '../hooks/usePlayer';
-import { useStage } from '../hooks/useStage';
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+import { useInterval } from "../hooks/useInterval";
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared)
 
-  console.log('re-render');
+  console.log("re-render");
 
-  const movePlayer = dir => {
+  const movePlayer = (dir) => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 });
     }
-  }
+  };
 
   const startGame = () => {
-    console.log("test")
+    console.log("test");
     // Reset everything
+    setDropTime(1000);
     setStage(createStage());
     resetPlayer();
     setGameOver(false);
-  }
+    setScore(0)
+    setRows(0)
+    setLevel(0)
+  };
 
   const drop = () => {
+    //increase the level when player has cleared 5 rows
+    if(rows > (level + 1) * 10){
+      setLevel(prev => prev + 1);
+      //also increase the speed by like .1 or whatever
+      setDropTime(1000 / (level + 1) + 200)
+    }
+
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-      updatePlayerPos({ x: 0, y: 1, collided: false })
+      updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
       // Game Over
       if (player.pos.y < 1) {
@@ -49,11 +63,20 @@ const Tetris = () => {
       }
       updatePlayerPos({ x: 0, y: 0, collided: true });
     }
-  }
+  };
+
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000 / (level + 1) + 200);
+      }
+    }
+  };
 
   const dropPlayer = () => {
+    setDropTime(null);
     drop();
-  }
+  };
 
   const move = ({ keyCode }) => {
     if (!gameOver) {
@@ -63,14 +86,23 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
-      } else if (keyCode === 38){
-        playerRotate(stage, 1)
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
-  }
+  };
+
+  useInterval(() => {
+    drop();
+  }, dropTime);
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={(e) => move(e)}
+      onKeyUp={keyUp}
+    >
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -78,9 +110,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
-              <Display text="Score" />
-              <Display text="Rows" />
-              <Display text="Level" />
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
             </div>
           )}
           <StartButton callback={startGame} />
